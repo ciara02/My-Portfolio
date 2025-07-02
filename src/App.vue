478614785d5a61e0aae4f3b5f55553d079e701ee
Ciara Dymosco <107288387+ciara02@users.noreply.github.com>
2025-07-02 09:56:12 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="bg-[#111827] min-h-screen">
+  <NavBar/>
+  <HeroSection/>
+  <ServicesSection/>
+  <AboutSection/>
+  <ExperienceAndSkills/>
+  <LatestProjectSection/>
+  <ContactSection/>
+</div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from "vue";
+// import HeroSection from '@/components/HeroSection.vue'
+const NavBar = defineAsyncComponent(() => import('@/components/NavBar.vue'));
+const HeroSection = defineAsyncComponent(() => import('@/components/HeroSection.vue')); 
+const ServicesSection = defineAsyncComponent(() => import('@/components/ServicesSection.vue'));  
+const AboutSection = defineAsyncComponent(() => import('@/components/AboutSection.vue'));  
+const ContactSection = defineAsyncComponent(() => import('@/components/ContactSection.vue'));  
+const ExperienceAndSkills = defineAsyncComponent(() => import('@/components/ExperienceAndSkills.vue'));  
+const LatestProjectSection = defineAsyncComponent(() => import('@/components/LatestProjectSection.vue'));  
+</script>
